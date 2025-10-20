6db0064e271267bf2f805e65179f87ad485b4667
dyiiinyi <andrii.kruk.600@gmail.com>
2025-10-20 17:36:25 +0200

    add header markup

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,16 @@
-<template></template>
-
-<script></script>
+<template>
+    <header>
+        <div class="container">
+            <ul>
+                <li><a href="#play">Play</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#rules">Rules</a></li>
+                <li><a href="#contacts">Contants</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
 
 <style scoped></style>
+
+<script></script>
